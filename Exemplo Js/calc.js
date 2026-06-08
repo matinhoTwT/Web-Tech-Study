d9196c911e1733a98ff1fc2cfc98b942a96e65e6
matinhoTwT <pedro.h.cruz8@edu.senai.br>
2026-06-08 11:23:33 -0300

    feat: adicionar arquivos de exercício

---
+++ Exemplo Js/calc.js
@@ -0,0 +1,15 @@
+// Variaveis
+let num1 = 10;
+let num2 = 10;
+
+// Operções
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+ 
+// Resultado
+console.log("Soma: " + soma);
+console.log("Subtração: " + sub);
+console.log("Multiplicação: " + mult);
+console.log("Divisão: " + div);
