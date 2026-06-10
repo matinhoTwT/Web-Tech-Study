aa5e1b8ad88e03ea7000a1bf913db34fd01aee96
matinhoTwT <pedro.h.cruz8@edu.senai.br>
2026-06-10 09:04:26 -0300

    Atualização

---
+++ Exemplo Js/calc2.js
@@ -0,0 +1,13 @@
+let num1 = Number(prompt("Digite o primeiro número:"));
+let num2 = Number(prompt("Digite o segundo número:"));
+
+// Operaçoes matemáticas
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2; 
+
+console.log(soma);  
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
